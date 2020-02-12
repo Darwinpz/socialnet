ce9aed35b85bb1ae8997b0cfb87d35be94688693
Darwin Pilaloa Zea <dpilaloa1@utmachala.edu.ec>
2020-02-11 22:32:21 -0500

    Principal
    Diseño deslizante

---
+++ frontend/src/components/historias.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class historias extends Component {
+    render() {
+        return (
+            <div className="card mb-3">
+
+                <small className="text-muted font-weight-bold">Historias</small>
+
+                <div className="container">
+
+                    
+                </div>
+
+            </div>
+        )
+    }
+}
