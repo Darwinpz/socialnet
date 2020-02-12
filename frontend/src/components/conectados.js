65de5edda03096fcab5ce2f51ba3c073390fd857
Gabriela Astudillo <35819215+gabiastudillo@users.noreply.github.com>
2020-02-11 22:40:15 -0500

    Panel de usuarios conectados
    Se crearon dos componentes: el contenedor de los usuarios conectados y el item de usuario que contiene un boton con la imagen de perfil del usuario y el nombre del usuario.

---
+++ frontend/src/components/conectados.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class conectados extends Component {
+    render() {
+        <div className="col flex-column float-lg-right float-md-right float-xl-right border bg-white m-0 ml-1 p-0">
+                <h6 className="bg-light p-2 m-0">CONECTADOS</h6>
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+                <UserItem />
+            </div>
+    }
+}
