7f4c280cf1060bff9d9d29123a491c7eaeaea3e8
Darwin Pilaloa Zea <dpilaloa1@utmachala.edu.ec>
2020-02-12 09:50:02 -0500

    Update conectados.js

--- frontend/src/components/conectados.js
+++ frontend/src/components/conectados.js
@@ -4,7 +4,7 @@ import UserItem from './useritem'
 export default class conectados extends Component {
     render() {
         return(
-        <div className="col flex-column float-lg-right float-md-right float-xl-right border bg-white m-0 ml-1 p-0">
+<div className="col flex-column float-lg-right float-md-right float-xl-right border bg-white m-0 ml-1 p-0">
                 <h6 className="bg-light p-2 m-0">CONECTADOS</h6>
                 <UserItem />
                 <UserItem />
