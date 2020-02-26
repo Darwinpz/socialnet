a9c57079bae0c951ca596b647dfd7bca57fa86a0
Silvia <scabrera@utmachala.edu.ec>
2020-02-25 20:50:42 -0500

    Messages 2
    Agregue subir imagen y otros iconos

--- frontend/src/components/messages.js
+++ frontend/src/components/messages.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Send from '@material-ui/icons/Send'
+import Abajo from '@material-ui/icons/ArrowDropDown'
+import Face from '@material-ui/icons/TagFaces'
+import File from '@material-ui/icons/InsertPhoto'
+import { Link } from 'react-router-dom'
 
 export default class messages extends Component {
 
     state = {
         search: ""
     }
+
+    loadimage(event) {
+        const output = document.getElementById('output');
+        output.src = URL.createObjectURL(event.target.files[0]);
+    };
+
 
     onInputChange = (e) => {
 
@@ -18,6 +29,13 @@ export default class messages extends Component {
     }
 
     render() {
+
+        const picturstyle = {
+            'object-fit': 'cover',
+            'object-position': 'center center'
+
+        } 
+
         return (
             <div className="container-fluid mt-5">
                 <div className="row">
@@ -37,7 +55,11 @@ export default class messages extends Component {
                                     <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
                                     <p className="text-muted small">Hey, you're arrested!</p>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
+                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "40px" }} class="time text-muted small small">13:21</span>
+                                <p className="dropdown abajo mt-1" type="button" data-toggle="dropdown"><Abajo /></p>
+                                <div class="dropdown-menu">
+                                    <Link className="dropdown-item">Eliminar mensaje</Link>
+                                </div>
                             </div>
                             <hr className="mt-1" />
                             <div style={{ padding: "5px", verticalAlign: "baseline", display: "flex", cursor: "pointer" }}>
@@ -46,7 +68,11 @@ export default class messages extends Component {
                                     <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
                                     <p className="text-muted small">Hey, you're arrested!</p>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
+                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "40px" }} class="time text-muted small small">13:21</span>
+                                <p className="dropdown abajo mt-1" type="button" data-toggle="dropdown"><Abajo /></p>
+                                <div class="dropdown-menu">
+                                    <Link className="dropdown-item">Eliminar mensaje</Link>
+                                </div>
                             </div>
                             <hr className="mt-1" />
                             <div style={{ padding: "5px", verticalAlign: "baseline", display: "flex", cursor: "pointer" }}>
@@ -55,7 +81,11 @@ export default class messages extends Component {
                                     <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
                                     <p className="text-muted small">Hey, you're arrested!</p>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
+                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "40px" }} class="time text-muted small small">13:21</span>
+                                <p className="dropdown abajo mt-1" type="button" data-toggle="dropdown"><Abajo /></p>
+                                <div class="dropdown-menu">
+                                    <Link className="dropdown-item">Eliminar mensaje</Link>
+                                </div>
                             </div>
                             <hr className="mt-1" />
                             <div style={{ padding: "5px", verticalAlign: "baseline", display: "flex", cursor: "pointer" }}>
@@ -64,7 +94,11 @@ export default class messages extends Component {
                                     <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
                                     <p className="text-muted small">Hey, you're arrested!</p>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
+                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "40px" }} class="time text-muted small small">13:21</span>
+                                <p className="dropdown abajo mt-1" type="button" data-toggle="dropdown"><Abajo /></p>
+                                <div class="dropdown-menu">
+                                    <Link className="dropdown-item">Eliminar mensaje</Link>
+                                </div>
                             </div>
                             <hr className="mt-1" />
                             <div style={{ padding: "5px", verticalAlign: "baseline", display: "flex", cursor: "pointer" }}>
@@ -73,16 +107,11 @@ export default class messages extends Component {
                                     <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
                                     <p className="text-muted small">Hey, you're arrested!</p>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
-                            </div>
-                            <hr className="mt-1" />
-                            <div style={{ padding: "5px", verticalAlign: "baseline", display: "flex", cursor: "pointer" }}>
-                                <img class="profile-image mt-2 mr-2" src="https://clarity-enhanced.net/wp-content/themes/clarity-enhanced/assets/img/bootstrap-chat-app-assets/robocop.jpg" style={{ width: "50px", height: "50px", borderRadius: "40px" }} alt="" />
-                                <div class="text">
-                                    <h6 className="usuarioname" style={{ marginTop: "11px", marginBottom: "0px" }}>Robo Cop</h6>
-                                    <p className="text-muted small">Hey, you're arrested!</p>
+                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "40px" }} class="time text-muted small small">13:21</span>
+                                <p className="dropdown abajo mt-1" type="button" data-toggle="dropdown"><Abajo /></p>
+                                <div class="dropdown-menu">
+                                    <Link className="dropdown-item">Eliminar mensaje</Link>
                                 </div>
-                                <span style={{ marginTop: "11px", marginBottom: "0px", marginLeft: "60px" }} class="time text-muted small small">13:21</span>
                             </div>
                             <hr className="mt-1" />
                         </div>
@@ -95,9 +124,10 @@ export default class messages extends Component {
                                 <h6 className="usuarioname-chat" style={{ marginTop: "11px", marginBottom: "3px", cursor: "pointer" }}>Robo Cop</h6>
                                 <p className="text-muted-chat small" style={{ color: "grey" }}>Activo hace 8min</p>
                             </div>
-                           
+
                         </div>
-                        
+
+
                         <div className="zona-chat" style={{ overflowY: "scroll", height: "480px", borderBottom: "1px solid rgb(211,211,211)" }}>
                             <ul className="chat mt-4">
                                 <li className="him">Hola silvia</li>
@@ -106,12 +136,38 @@ export default class messages extends Component {
                                 <li className="me">Gracias Robo</li>
                                 <li className="me">Te escribo luego men</li>
                                 <li className="him">Ya pilas</li>
-                                <li className="him">La titulación es un proceso de concreción de competencias profesionales y de investigación, donde el estudiante tiene la oportunidad de demostrar sus conocimientos profesionales siendo de apoyo para la sociedad con trabajos de investigación.</li>
+                                <li className="him"><img class="profile-image mt-2 mr-2" src="https://clarity-enhanced.net/wp-content/themes/clarity-enhanced/assets/img/bootstrap-chat-app-assets/robocop.jpg" alt="" /></li>
                             </ul>
                         </div>
 
                         <div className="row">
-                            <input className="form-control mt-3 ml-2 mr-5" type="text" style={{ background: "#eee", borderRadius: "40px" }} placeholder="Escribe un mensaje" /> 
+                            <div className="col-10">
+                                <input className="form-control mt-3 ml-2 mr-5" type="text" style={{ background: "#eee", borderRadius: "40px" }} placeholder="Escribe un mensaje" />
+                            </div>
+                            <p className="ml-2 mt-4 mr-2" type="button"><Send /></p>
+                            <p className="mt-4 mr-2" type="button"><Face /></p>
+                            <p className="mt-4 mr-2" type="button" data-toggle="modal" data-target="#exampleModalCenter"><File /></p>
+
+                            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLongTitle">Subir archivos</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body" style={{alignContent:"center"}}>
+                                            <input type="file" onChange={this.loadimage.bind(this.event)} />
+                                            <img src="" id="output" width="200" height="200" style={picturstyle} alt=""/>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                            <button type="button" class="btn btn-primary">Enviar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
                         </div>
 
